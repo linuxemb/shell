0ed3ba03cbaf444591788d3d687b32fc403c39e0
LSHI34 <LSHI34@ford.com>
2020-01-18 22:36:23 -0500

    another way of getops

---
+++ LCLSSBed3/getOP.sh
@@ -0,0 +1,20 @@
+
+#!/bin/shell
+while getopts ae:f:hd:s:qx: option
+do
+        case "${option}"
+        in
+                
+		a) echo "found  a option"
+		echo done ;;
+
+                e) ADMIN=${OPTARG};;
+                d) DOMAIN=${OPTARG};;
+                f) SERVERFILE=$OPTARG;;
+                s) WHOIS_SERVER=$OPTARG;;
+                q) QUIET="TRUE";;
+                x) WARNDAYS=$OPTARG;;
+                \?) usage
+                    exit 1;;
+        esac
+done

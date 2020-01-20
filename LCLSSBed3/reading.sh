8fb4ecf799e411516e5e123de2dc31f49be6f4f0
LSHI34 <LSHI34@ford.com>
2020-01-19 21:40:53 -0500

    read from user and file

--- LCLSSBed3/reading.sh
+++ LCLSSBed3/reading.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 ## n1 --> only accetp a singal charactoer before exiting 
-
+#  -s silence read,  
 read -n1 -p "Do you like to continue" answer
 case  $answer in
 	Y|y)echo 
@@ -11,4 +11,38 @@ case  $answer in
 		exit;;
 esac
 echo "end of your script"
+
+#################  Read from file 
+count =1
+cat test | while read line
+do
+	echo "line $count:$line"
+	let count++
+done
+echo "Finished processing file"
+
+
+###########33333333333333
+#$ ./reading.sh
+#Do you like to continuey
+fine contnue..
+end of your script
+./reading.sh: line 17: count: command not found
+line :
+line 1:
+line 2:#!/bin/bash
+line 3:##### geti option  same way as parameter val, use case srtatement to determine when a parameter is formateded as an option
+line 4:
+line 5:while [ -n "$1" ]
+line 6:do
+line 7:case "$1" in
+line 8:-a) echo "found the a option";;
+line 9:-b) echo "fount the -bopoint";;
+line 10:-c) echo "fount the -c opoint";;
+line 11:*) echo "$1 is not an option ";;
+line 12:esac
+line 13:shift
+line 14:done
+line 15:
+Finished processing file
 
